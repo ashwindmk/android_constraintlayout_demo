24b563a74ace3ea379edb8b59b0467ce528712b2
Ashwin Dinesh <ashwin.dinesh01@gmail.com>
2020-03-15 16:53:58 +0530

    Add guidelines demo

--- app/src/main/java/com/ashwin/android/constraintlayoutdemo/MainActivity.kt
+++ app/src/main/java/com/ashwin/android/constraintlayoutdemo/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.ashwin.android.constraintlayoutdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openGuidelineActivity(view: View) {
+        startActivity(Intent(this, GuidelineActivity::class.java))
+    }
+
+    fun exit(view: View) {
+        finish()
     }
 }
